a34c44efbee89e4ed59d19a760e7d48f2d2c4c29
Bert Roex <>
2019-08-02 22:33:56 +0200

    Revert "Added oh my zsh"
    This reverts commit d76b31f4

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -77,10 +77,6 @@ defaults write org.m0k.transmission RandomPort -bool true
 
 brew cask install iterm2
 
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# TODO add custom .zshrc
-
 # Browsers
 
 brew cask install google-chrome
