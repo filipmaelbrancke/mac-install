db848227460f7719f0f3f51b27a0a523cdcac953
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:39:10 +0100

    Modify Android installation

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,11 +1,11 @@
 echo
 echo "Installing Android tools"
 brew cask install android-studio
-brew cask install android-file-transfer
+# brew cask install android-file-transfer
 brew cask install jd-gui
 brew install apktool
-brew install dex2jar
+# brew install dex2jar
 brew install jadx
-brew cask install realm-browser
+# brew cask install realm-browser
 brew install scrcpy
-brew cask install android-platform-tools
+# brew cask install android-platform-tools
