8cacb540405792bc3b0826b43829c9908f400138
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:39:33 +0100

    Docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,9 +1,11 @@
 echo
 echo "Installing Docker"
-brew install --cask docker
+brew cask install docker
+# brew cask install docker-toolbox
 
 # Dive == tool for exploring a docker image, layer contents, and discovering ways to shrink your Docker image size
-brew install dive
+# brew tap wagoodman/dive
+# brew install dive
 
-# Amazon ECR Docker Credential Helper: https://github.com/awslabs/amazon-ecr-credential-helper#Configuration
-brew install docker-credential-helper-ecr
+# Amazon ECR Docker Credential Helper
+# brew install docker-credential-helper-ecr
