a28c8bc6a2e1b324e36a58271ce41ad30b03cddf
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:30:55 +0100

    Only install programming fonts

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -6,19 +6,19 @@ echo "Installing fonts"
 # Installing fonts tap
 brew tap homebrew/cask-fonts
 
-brew cask install font-inconsolata
-brew cask install font-roboto
-brew cask install font-roboto-mono
-brew cask install font-roboto-slab
-brew cask install font-source-code-pro
-brew cask install font-overpass
-brew cask install font-montserrat
-brew cask install font-londrina-outline
-brew cask install font-londrina-shadow
-brew cask install font-londrina-sketch
-brew cask install font-londrina-solid
-brew cask install font-lobster
-brew cask install font-source-sans-pro
+# brew cask install font-inconsolata
+# brew cask install font-roboto
+# brew cask install font-roboto-mono
+# brew cask install font-roboto-slab
+# brew cask install font-source-code-pro
+# brew cask install font-overpass
+# brew cask install font-montserrat
+# brew cask install font-londrina-outline
+# brew cask install font-londrina-shadow
+# brew cask install font-londrina-sketch
+# brew cask install font-londrina-solid
+# brew cask install font-lobster
+# brew cask install font-source-sans-pro
 
 # Fonts with programming ligatures
 brew cask install font-fira-code
