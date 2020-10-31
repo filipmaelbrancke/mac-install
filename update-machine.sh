bf53ab9629a05653e5727b4f17aeca35ad1a5450
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 13:11:18 +0100

    update update-machine.sh

--- update-machine.sh
+++ update-machine.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 START_DATE=$(date +"%d.%m.%Y | %H:%M:%S")
 SECONDS=0
 echo "--- Updater for this machine started at: $START_DATE ---"
@@ -18,19 +17,6 @@ brew cleanup
 echo “you can hit mas upgrade to upgrade theses apps from the app store:”
 mas outdated
 echo “install with: mas upgrade”
-
-echo
-echo "- Updating \"apm\" -"
-apm update --no-confirm
-apm clean
-
-echo
-echo "- Updating \"npm\" -"
-npm update -g
-
-echo
-echo "- Updating \"homebrew casks (via the brew-cask-upgrade tool)\" -"
-brew cu --cleanup
 
 END_DATE=$(date +"%d.%m.%Y | %H:%M:%S")
 ELAPSED=" $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
