b855b130b2974f6077c7e58c4b5214328a75bca2
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:42:49 +0100

    General dev

--- scripts/general-dev.sh
+++ scripts/general-dev.sh
@@ -2,31 +2,31 @@ echo
 echo "Installing general development tools"
 
 # command line HTTP client
-brew install httpie
+# brew install httpie
 
 # command line http benchmarking
-brew install siege
+# brew install siege
 
 # API testing tool
-brew cask install postman
-brew cask install insomnia
-brew cask install insomnia-designer
-brew cask install firecamp
+# brew cask install postman
+# brew cask install insomnia
+# brew cask install insomnia-designer
+# brew cask install firecamp
 
 # Secure tunnels to localhost
 brew cask install ngrok
 
 # DB
 ## GUIs
-brew cask install sequel-pro # mysql
-brew cask install robo-3t # mongo
+# brew cask install sequel-pro # mysql
+# brew cask install robo-3t # mongo
 brew cask install db-browser-for-sqlite # sqlite
-brew cask install postico # postgresql
+# brew cask install postico # postgresql
 
 #brew cask install dash
 
-brew cask install graphiql
-brew cask install mqtt-explorer
+# brew cask install graphiql
+# brew cask install mqtt-explorer
 
 # Virtual
 
@@ -36,8 +36,8 @@ brew cask install virtualbox
 
 # Docker containers instead of locally installed servers
 
-brew install vault
+# brew install vault
 
-brew install jq
-brew install coreutils
-brew install graphviz
+# brew install jq
+# brew install coreutils
+# brew install graphviz
