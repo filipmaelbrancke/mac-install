de116cf4699f0a2c7d51a42d4cadce8596ab1915
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:30:55 +0100

    Only install programming fonts

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,31 +1,26 @@
 echo
 echo "Installing fonts"
 
+# Fonts
+
 # Installing fonts tap
 brew tap homebrew/cask-fonts
 
-# In the latest versions of XCode, svn is deprecated and no longer seems to work correctly. If this is universally true, install svn for the svn dependent font casks.
-brew install subversion
-
-# Fonts
-
-brew install font-inconsolata
-brew install font-roboto
-brew install font-roboto-mono
-brew install font-roboto-slab
-brew install font-source-code-pro
-brew install font-overpass
-brew install font-montserrat
-brew install font-londrina-outline
-brew install font-londrina-shadow
-brew install font-londrina-sketch
-brew install font-londrina-solid
-brew install font-lobster-two
-brew install font-source-sans-pro
+# brew cask install font-inconsolata
+# brew cask install font-roboto
+# brew cask install font-roboto-mono
+# brew cask install font-roboto-slab
+# brew cask install font-source-code-pro
+# brew cask install font-overpass
+# brew cask install font-montserrat
+# brew cask install font-londrina-outline
+# brew cask install font-londrina-shadow
+# brew cask install font-londrina-sketch
+# brew cask install font-londrina-solid
+# brew cask install font-lobster
+# brew cask install font-source-sans-pro
 
 # Fonts with programming ligatures
-
-brew install font-jetbrains-mono
-brew install font-fira-code
-brew install font-hasklig
-brew install font-monoid
+brew cask install font-fira-code
+brew cask install font-hasklig
+brew cask install font-monoid
