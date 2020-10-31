a29c4052e74b28723887e1ffd332ab416e09d680
timrijckaert <tim.rijckaert@gmail.com>
2020-10-31 14:38:39 +0100

    Installing ZSH

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,5 +1,6 @@
+echo "Installing Oh-My-Zsh"
 # Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # ZSH Autosuggestions
 brew install zsh-autosuggestions
