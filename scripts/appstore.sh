8675d53846ed863990734be0ca481731accb4ae9
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:51:45 +0100

    Install Airmail from Mac App Store

--- scripts/appstore.sh
+++ scripts/appstore.sh
@@ -4,18 +4,8 @@ function installMacAppStoreApps {
   # mas-cli = command line interface for the Mac App Store
   brew install mas
   # apps are installed with their Mac App Store product identifier
-  # 'Pixelmator', id: 407963104
-  mas install 407963104
-  # 'Spark', id: 1176895641
-  mas install 1176895641
-  # 'Amphetamine - Powerful keep-awake utility', id: 937984704
-  mas install 937984704
-  # 'WireGuard VPN client', id: 1451685025
-  mas install 1451685025
-  # 'Pixelmator Pro', id: 1289583905
-  mas install 1289583905
-  # 'Byword - Markdown editor', id: 420212497
-  mas install 420212497
+  # 'Airmail', id: 993160329
+  mas install 993160329
 }
 
 
