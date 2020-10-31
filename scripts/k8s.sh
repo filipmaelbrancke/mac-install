0f0529067f09b4650660098f4da6a2e35933ed17
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:41:49 +0100

    Remove k8s

--- scripts/k8s.sh
+++
@@ -1,8 +0,0 @@
-echo
-echo "Installing Kubernetes related"
-brew install helm@3
-brew install minikube
-brew cask install kitematic
-brew install octant
-brew install derailed/k9s/k9S
-brew cask install lens
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -39,7 +39,6 @@ source ${MY_DIR}/cli.sh
 source ${MY_DIR}/applications.sh
 source ${MY_DIR}/fonts.sh
 source ${MY_DIR}/docker.sh
-source ${MY_DIR}/k8s.sh
 source ${MY_DIR}/general-dev.sh
 source ${MY_DIR}/appstore.sh
 source ${MY_DIR}/configurations.sh
