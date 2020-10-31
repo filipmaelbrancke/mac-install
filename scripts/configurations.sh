999287862aef4f5aeff4fd02fb2407b5f2defb2d
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:49:39 +0100

    Clean up configuration and config files

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -1,32 +1,6 @@
 echo
 # prereqs
 
-# OS X Sierra prevents changes to TCC.db, so only enable tccutil when SIP is disabled
-#brew install tccutil
-# add loginitems repo https://github.com/OJFord/loginitems
-brew tap OJFord/formulae
-brew install loginitems
-
 echo "Configuring iTerm"
-cp files/com.googlecode.iterm2.plist ~/Library/Preferences
+# cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 
-echo "Configuring Rectangle"
-#loginitems -a "Rectangle" -p "/Applications/Rectangle.app" # Start on login
-open /Applications/Rectangle.app
-echo "Installing Rectangle shortcuts"
-ditto files/com.knollsoft.Rectangle.plist ~/"Library/Preferences/com.knollsoft.Rectangle.plist"
-
-echo "Configuring FlyCut"
-loginitems -a "Flycut" -p "/Applications/Flycut.app" # Start at login
-open /Applications/Flycut.app
-
-echo "Installing QuickLook extensions"
-brew cask install qlstephen  # QLStephen is a QuickLook plugin to view plain text files without a file extension
-#brew cask install webpquicklook # WebP
-brew cask install quicklookapk # Preview Android APK files
-brew cask install qlcolorcode # Renders source code with syntax highlighting
-brew cask install qlmarkdown # Renders a preview of a Markdown file
-brew cask install quicklook-json # Renders a preview of a Json file
-brew cask install qlimagesize # Display image size and resolution
-brew cask install suspicious-package # Preview the contents of a standard Apple installer package
-brew cask install quicklookase # Preview Adobe ASE Color Swatches
