0f678d50e4e3c4a9095ace9b3ffa1b22447075e5
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 12:32:42 +0100

    Disable heroku, confuentinc, ccloud and Azure cli installation

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -2,14 +2,16 @@ echo
 echo "Installing CLIs"
 # Google Cloud SDK
 brew cask install google-cloud-sdk
+
 # Heroku CLI
-brew tap heroku/brew
-brew install heroku
+# brew tap heroku/brew
+# brew install heroku
+
 # Firebase Command Line Interface
 #npm -g install firebase-tools
 brew install awscli
 
-brew tap confluentinc/ccloud
-brew install ccloud
+# brew tap confluentinc/ccloud
+# brew install ccloud
 
-brew install azure-cli
+# brew install azure-cli
