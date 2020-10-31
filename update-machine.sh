eb0fc2462f923f4d76a372eadcedad396a0530c1
timrijckaert <tim.rijckaert@nowmax.be>
2020-10-31 13:11:18 +0100

    update update-machine.sh

--- update-machine.sh
+++ update-machine.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 START_DATE=$(date +"%d.%m.%Y | %H:%M:%S")
 SECONDS=0
 echo "--- Updater for this machine started at: $START_DATE ---"
@@ -10,7 +9,7 @@ echo "- Updating \"macOS\" -"
 softwareupdate --install --all
 
 echo
-echo "- Updating \"brew\" formulas and casks -"
+echo "- Updating \"brew\" -"
 brew update
 brew upgrade
 brew cleanup
@@ -18,20 +17,6 @@ brew cleanup
 echo “you can hit mas upgrade to upgrade theses apps from the app store:”
 mas outdated
 echo “install with: mas upgrade”
-
-echo
-echo "- Updating \"SDKMAN installed SDKs\" -"
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk upgrade
-
-echo
-echo "- Updating \"apm\" -"
-apm update --no-confirm
-apm clean
-
-echo
-echo "- Updating \"npm\" -"
-npm update -g
 
 END_DATE=$(date +"%d.%m.%Y | %H:%M:%S")
 ELAPSED=" $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
