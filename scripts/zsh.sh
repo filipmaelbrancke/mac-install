0c38daed341a17eac3ea4275cccbe4b2fed45de5
timrijckaert <tim.rijckaert@gmail.com>
2020-10-31 20:07:16 +0100

    Copy .zshrc from private config script folder

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,6 +1,16 @@
 echo "Installing Oh-My-Zsh"
+
+if [ -z "$configscriptpath" ]; then
+    CONFIG_SCRIPT_PATH=$1
+else
+    CONFIG_SCRIPT_PATH=$configscriptpath
+fi
+
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # ZSH Autosuggestions
 brew install zsh-autosuggestions
+
+# Copy custom .zshrc
+cp "$CONFIG_SCRIPT_PATH/$HOME/.zshrc" "~/.zshrc"
