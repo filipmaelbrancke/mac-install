26cad0ce113ffabbb32e0f94bada38360aaddfb6
Tim Rijckaert <tim.rijckaert@gmail.com>
2022-09-24 15:16:56 +0200

    Convert project to use Ansible

--- after-setup-customization.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Bonjour name ending in .local
-scutil --set LocalHostName "0x66696c6970"
-# Friendly name shown in System Preferences > Sharing
-scutil --set ComputerName "0x66696c6970"
-# The name recognized by the hostname command
-scutil --set HostName "0x66696c6970"
